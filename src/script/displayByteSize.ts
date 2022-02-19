0e5d6df2db170c69364a6688476c77770b36a079
Frank Mayer <frank.mayer1907@protonmail.com>
2022-02-19 18:19:04 +0100

    files as grid view

--- src/script/displayByteSize.ts
+++ src/script/displayByteSize.ts
@@ -1,11 +1,11 @@
 export const displayByteSize = (bytes: number, precision = 4): string => {
   if (bytes > 1024) {
     if (bytes > 1048576) {
-      return `${(bytes / 1048576).toPrecision(precision)} MiB`;
+      return `${(bytes / 1048576).toPrecision(precision)} MiB`;
     } else {
-      return `${(bytes / 1024).toPrecision(precision)} KiB`;
+      return `${(bytes / 1024).toPrecision(precision)} KiB`;
     }
   } else {
-    return `${bytes.toPrecision(precision)} B`;
+    return `${bytes.toPrecision(precision)} B`;
   }
 };
